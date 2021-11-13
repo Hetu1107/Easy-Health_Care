17ca795f213c9cab340dc8c21e0056fc2c69a48e
Hetu Patel <hetu200211@gmail.com>
2021-11-13 14:06:24 +0530

    PrivateChat Box Added

--- client/src/components/User/PrivateChat.js
+++ client/src/components/User/PrivateChat.js
@@ -1,9 +1,42 @@
 import React from 'react'
 
+
+const chat_doctors = [
+    {
+        no : 1,
+        name : 'Dr Hetu Patel',
+        qualifications : 'Secondary Physician'
+    },
+    {
+        no : 2,
+        name : 'Dr. Natik Patil',
+        qualifications :'MBBS Pathologist'
+    },
+    {
+        no : 3,
+        name : 'Dr. Zaid Bhimala',
+        qualifications : 'MD Surgery Expert'
+    }
+]
 function PrivateChat() {
     return (
-        <div>
-            private chat
+        <div className="Private_Chat_Div">
+            {
+                chat_doctors.map(res=>{
+                    return(
+                        <div className="Private_Chat_Div_box">
+                            <div>
+                                <h2>Room : {res.no}</h2>
+                                <h3>Name : {res.name}</h3>
+                            </div>
+                            <div>
+                                <h3>Qualifications : {res.qualifications}</h3>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            
         </div>
     )
 }
